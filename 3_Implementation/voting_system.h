e6c71f2732b274f0c18ffac3c228a1153953d5be
Dineshkumar824 <98821041+Dineshkumar824@users.noreply.github.com>
2022-02-26 14:33:14 +0530

    Update voting_system.h

--- 3_Implementation/voting_system.h
+++ 3_Implementation/voting_system.h
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// total no of the parties
+#define total_no_of_parties
+#define party_no_1 "A"
+#define party_no_2 "B" 
+#define party_no_3 "C" 
+#define party_no_4 "D
