c52f40d672e34777e2aa56761e88a607050b18b1
Dineshkumar824 <98821041+Dineshkumar824@users.noreply.github.com>
2022-03-04 13:48:15 +0530

    Update header.h

--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -1,5 +1,10 @@
-int vote_Count1 = 0,
-    vote_Count2 = 0,
-    vote_Count3 = 0,
-    vote_Count4 = 0,
-    invalid_votes = 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+// define macroname charseq
+// total no of the parties
+#define total_no_of_parties
+#define party_no_1 "A"
+#define party_no_2 "B" 
+#define party_no_3 "C" 
+#define party_no_4 "D"
